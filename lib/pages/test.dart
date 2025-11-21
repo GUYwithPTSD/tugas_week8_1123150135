41079e7e35b7984a22a7ed91c892b3bc004a9007
GUYwithPTSD <ivandarma3000@gmail.com>
2025-11-21 21:19:03 +0700

    ketinggalan ngoding 2

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -6,60 +6,67 @@ class TestPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Card(
-                elevation: 8,
-                color: Colors.red,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Text(
-                    "Card with color",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
+      appBar: AppBar(title: const Text("contoh dashboard")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
 
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12.0),
-                  color: Colors.blue,
-                ),
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              color: Colors.lightBlueAccent,
+              elevation: 8,
+              child: ListTile(
+                leading: Icon(Icons.account_circle, size: 50),
+                title: Text("Latihan Card 1"),
+                subtitle: Text("Ini adalah contoh latihan card pertama"),
+              ),
+            ),
+            Card(
+              elevation: 8,
+              color: Colors.red,
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
                 child: Text(
-                  "Container with color",
-                  style: TextStyle(fontSize: 16.0),
+                  "Card with color",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
                 ),
               ),
+            ),
 
-              Card(
-                elevation: 8,
-                color: Colors.grey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Tittle", style: TextStyle(fontSize: 20)),
-                    SizedBox(height: 12),
-                    Text(
-                      "Laga Terakhir Midoriya Izuku",
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey[700],
-                        height: 1.5,
-                      ),
-                      textAlign: TextAlign.justify,
-                    ),
-                  ],
-                ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+                color: Colors.blue,
               ),
-            ],
-          ),
+              child: Text(
+                "Container with color",
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+
+            Card(
+              elevation: 8,
+              color: Colors.grey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Tittle", style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 12),
+                  Text(
+                    "Laga Terakhir Midoriya Izuku",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[700],
+                      height: 1.5,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
