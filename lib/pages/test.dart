7e577a047bdc4f7d2ad9b6c8f0c5f8dcb77ca1ed
GUYwithPTSD <ivandarma3000@gmail.com>
2025-11-21 21:10:24 +0700

    ketinggalan ngoding

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -13,12 +13,49 @@ class TestPages extends StatelessWidget {
           child: Column(
             children: [
               Card(
-                color: Colors.lightBlueAccent,
                 elevation: 8,
-                child: ListTile(
-                  leading: Icon(Icons.account_circle, size: 50),
-                  title: Text("Card 1"),
-                  subtitle: Text("contoh card 1"),
+                color: Colors.red,
+                child: Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text(
+                    "Card with color",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  "Container with color",
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+
+              Card(
+                elevation: 8,
+                color: Colors.grey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Tittle", style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 12),
+                    Text(
+                      "Laga Terakhir Midoriya Izuku",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[700],
+                        height: 1.5,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
                 ),
               ),
             ],
