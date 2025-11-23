7cdf2c548f2b893893652ac80bcf5317ca77db3c
GUYwithPTSD <ivandarma3000@gmail.com>
2025-11-23 16:40:58 +0700

    membuat logo lingkaran kuning-merah

--- lib/pages/mastercard.dart
+++ lib/pages/mastercard.dart
@@ -74,6 +74,27 @@ class MasterCard extends StatelessWidget {
                                     color: Colors.white,
                                   ),
                                 ),
+                                Stack(
+                                  children: [
+                                    Container(
+                                      width: 30,
+                                      height: 30,
+                                      decoration: const BoxDecoration(
+                                        color: Color(0xFFEB001B),
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.only(left: 15),
+                                      width: 30,
+                                      height: 30,
+                                      decoration: const BoxDecoration(
+                                        color: Color(0xFFF79E1B),
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ],
                             ),
                           ],
