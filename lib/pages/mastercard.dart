c13a449f76f3118e4ea5215f7d68ecb73b4382a8
GUYwithPTSD <ivandarma3000@gmail.com>
2025-11-23 16:34:10 +0700

    membuat logo kedua  diatas

--- lib/pages/mastercard.dart
+++ lib/pages/mastercard.dart
@@ -6,7 +6,7 @@ class MasterCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Master Card"), actions: []),
+      appBar: AppBar(title: const Text("Master Card")),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(16.0),
